186392826829a1e4d8b4c2da9c68e03b78706a9c
socl0sed <force@gmail.com>
2024-05-07 12:43:24 +0000

    changed clang format

--- badcode.cpp
+++ badcode.cpp
@@ -2,8 +2,8 @@
 #include <string>
 
 int main() {
-	string name; 
-	std::cin >> name; //Request
-	std::cout << "Hello world from " << name << std::endl; //Answer
-	return 0;
+    string name;
+    std::cin >> name;                                      // Request
+    std::cout << "Hello world from " << name << std::endl; // Answer
+    return 0;
 }
